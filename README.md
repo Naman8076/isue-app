d7f4bb90b07da1f049fa6a2cf94981ffa9079a3b
naman <namansinghal2110@gmail.com>
2023-11-01 00:07:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Issue-Detector-Backend-Nodejs-ExpressJs-MongoDB
+Created an Issue Detector Web Application.
+
+## Users can do multiple tasks over here...
+* Create a project
+* Check the details of the project
+* Create a new issue for the bug present in the project
+* Filter the issues based on labels or Author Name
+* Search Issues based on Issue title and description.
+All this data will be stored at MongoDB Atlas.
+
+
+
+
---
+++ config/dbConnection.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+// Making Connecting with mongoDB
+const connectDb = async ()=>{
+    try{
+        // const connect = await mongoose.connect("mongodb+srv://namansinghal2110:8OujhVzpj5eMx2sY@cluster0.z1kliqf.mongodb.net/?retryWrites=true&w=majority")
+        // console.log("Database Connected: ", connect.connection.name)
+             const connect = await mongoose.connect("mongodb+srv://namansinghal2110:naDxWRJM3vQN8jBP@cluster0.nlackvx.mongodb.net/?retryWrites=true&w=majority")
+        console.log("Database Connected: ", connect.connection.name)
+        
+        // 8OujhVzpj5eMx2sY
+        // naDxWRJM3vQN8jBP
+    }
+    catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+// Exporting the function 
+module.exports = connectDb;
+// naDxWRJM3vQN8jBP
